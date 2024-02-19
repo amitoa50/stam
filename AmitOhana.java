dbf98dfd4be9b996265e5df760f95dff844ba388
Your Name <you@example.com>
2024-02-19 11:51:50 +0200

    shorter the loop

--- AmitOhana.java
+++ AmitOhana.java
@@ -1,7 +1,7 @@
 public class AmitOhana {
     public static void main(String [] args)
     {
-        for(int i=1;i<=10;i++)
+        for(int i=1;i<=5;i++)
         {
             System.out.println("Amit Ohana is the king...."+i);
         }
